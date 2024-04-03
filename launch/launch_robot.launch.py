4874025c6ae1b0a5b555fbdb833f245746a208c7
Althahir10 <127445963+Althahir10@users.noreply.github.com>
2024-04-03 13:00:29 -0500

    change

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -30,11 +30,7 @@ def generate_launch_description():
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
-    controller_params_file = os.path.join(
-        get_package_share_directory(my_bot), # <-- Replace with your package name
-        'config',
-        'my_controllers.yaml'
-        )
+    controller_params_file = os.path.join(get_package_share_directory(my_bot), 'config', 'my_controllers.yaml')
 
     controller_manager = Node(
         package="controller_manager",
